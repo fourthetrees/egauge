954ec5af590c196a42335fdd606b74f213f444f1
Forrest <forrest@csbcd.(none)>
2016-12-15 16:28:04 -1000

    added basic error logging to cron job

--- src/cron/egauge_cron.py
+++ src/cron/egauge_cron.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import subprocess as sp
 import os
 
 uri = "/home/forrest/Code/csbcd/egauge/"
 
 os.chdir(uri)
 
-os.system('./main.py')
+# os.system('./main.py')
+try:
+    sp.run('./main.py',check=True)
+except Exception as err:
+    with open('tmp/log.txt','a') as fp:
+        fp.write(err.stdout)
