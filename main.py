443eeb52c7d37c762e2f8111ba07e2197937fd05
forrest <fspmarshall@gmail.com>
2017-02-23 19:53:35 -1000

    improved error logging

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import json
 def run(project):
     os.chdir(dirname(__file__))
     config,nonce= get_params(project)
-    print('querying gauges...')
     data,nonce_new = query_gauges(config["gauges"],nonce)
     handle_data(data,config,project)
     update_nonce(project,nonce_new)
@@ -22,21 +21,16 @@ def run(project):
 # Do something with the data.
 def handle_data(data,config,project):
     if 'mapping' in config:
-        print('reformatting data...')
         data = fmt_mapping(data,config['mapping'])
-    if not data:
-        print('no data valid data... skipping save step.')
-        return
+    if not data: return
     if not 'export_fmt' in config:
         raise Exception('no export format specified for {}!'.format(project))
     export_config = { k.lower() : config['export_fmt'][k]
                     for k in config['export_fmt']      }
     for k in export_config:
         if k == 'csv':
-            print('saving {} rows to csv...'.format(len(data)))
             to_csv(data,project,export_config['csv'])
         elif k == 'psql':
-            print('saving {} rows to psql...'.format(len(data)))
             to_psql(data,project,export_config['psql'])
         else:
             raise Exception('unrecognized export format: {}'.format(k))
--- src/errlog.py
+++ src/errlog.py
@@ -2,15 +2,15 @@
 import time
 import csv
 import os
+import os.path as path
 
 # Generate and update a simple and readable log file.
 def mklog(project,text):
     hdr  = 'logtime............{}'.format(int(time.time()))
     proj = 'project............{}'.format(project)
     ftr  = '...................endlog\n'
-    if project in os.listdir('tmp/projects/'):
-        fpath = 'tmp/projects/{}/err_log.txt'.format(project)
-    else: fpath = 'tmp/err_log.txt'
+    errdir = dirset(project)
+    fpath  = dirset + 'errlog.txt'
     with open(fpath,'a') as fp:
         write = lambda txt: print(txt,file=fp)
         write(hdr)
@@ -21,13 +21,18 @@ def mklog(project,text):
 # write a csv of improperly formatted
 # data to an appropriate errors file.
 def fmterr(project,data,txt='fmterr'):
-    projdir = 'tmp/errors/{}/'.format(project)
-    if not project in os.listdir('tmp/errors/'):
-        os.mkdir(projdir)
-    fname = projdir + txt + '-'+ str(time.time()) + '.csv'
+    errdir = dirset(project)
+    fpath = errdir + txt + '-'+ str(int(time.time())) + '.csv'
     fields = data[0]._fields
-    with open(fname,'w') as fp:
+    with open(fpath,'w') as fp:
         print('writing {} malformed rows to: {}'.format(len(data),fname))
         writer = csv.writer(fp)
         writer.writerow(fields)
         for d in data: writer.writerow(d)
+
+# Setup error directories if necessary.
+def dirset(project):
+    errdir = 'tmp/errors/{}/'.format(project)
+    if not path.isdir(errdir):
+        os.makedirs(errdir)
+    return errdir
